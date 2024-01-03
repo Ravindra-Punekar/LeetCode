6b018c993b54725814fd6cb0e498c6c6e428221f
Ravindra Punekar <91467528+Ravindra-Punekar@users.noreply.github.com>
2024-01-03 12:39:01 +0530

    Time: 124 ms (11.04%), Space: 23.3 MB (82.87%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     int numberOfBeams(vector<string>& bank) {
+         ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int beams = 0;
-        map<int, int> mp;
-        for (int i = 0; i < bank.size(); i++)
-            for (auto cell : bank[i])
-                if (cell == '1')
-                    mp[i]++;
-
-        if(mp.size()<=1)  return 0;
-        auto it = mp.begin();
-        int prev = it->second;
-        it++;
-        while (it != mp.end()) {
-            beams += prev * it->second;
-            prev = it->second;
-            it++;
-        }
+        int prev = 0;
         
+        for(int i=0; i<bank.size(); i++){
+            int curr = count(bank[i].begin(),bank[i].end(),'1');
+            
+            if(prev!=0 && curr!=0)
+                beams += prev*curr;
+            if(curr!=0) 
+                prev = curr;
+        }
+
         return beams;
     }
 };
